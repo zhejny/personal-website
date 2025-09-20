fe459f84e73353a111ee8054fb3a20bdabb38182
zhejny <zachary.hejyn@gmail.com>
2025-09-20 18:24:39 +0800

    change about picture

--- components/about.tsx
+++ components/about.tsx
@@ -9,7 +9,7 @@ function About() {
     <section className="about">
       <div className="about-subsection">
         <Image
-          src="/assets/images/glitch.png"
+          src="/assets/images/taiwan train station.jpg"
           alt=""
           width={400} // 200
           height={300} // 314
